4822d677cd8ec85142444a9d0087a91afc8b8142
Alex <alextrotman04@gmail.com>
2023-08-19 12:21:39 -0500

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, str){
+    let found = drivers.filter((drivers => drivers.toLowerCase() === str.toLowerCase()))
+    return found
+}
+
+function fuzzyMatch(drivers, str){
+    let found = drivers.filter((drivers => drivers[1] === str[1]))
+    return found
+}
+
+function matchName(obj, str){
+    let found = obj.filter((obj => obj.name === str))
+    return found
+}
